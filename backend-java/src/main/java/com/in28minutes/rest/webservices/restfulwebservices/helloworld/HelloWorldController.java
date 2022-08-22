193344f62c134efba042711995aa9cf4168a78f3
DInesh Reddy Jetti <DRJetti@zuelligpharma.com>
2022-08-22 21:11:41 +0530

    test

---
+++ backend-java/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/tracker/check")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	///hello-world/path-variable/in28minutes
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		//throw new RuntimeException("Something went wrong");
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+
+    @GetMapping(path = "/tracker")
+	public boolean helloWorld1() {
+		return true;
+	}
+}
